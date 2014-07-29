354867eb76b38c1979f072cfbe4566a325b0475a
bjtice <bjtice@gmail.com>
2014-07-29 22:59:36 +0100

    Added support for I2C backback driving LCD screen

--- apd.ino
+++ apd.ino
@@ -1,14 +1,16 @@
-// APD version 1.1
-// 7/28/14
+// APD version 1.1.01
+// 7/29/14
 // Brian Tice
 
-#include "Wire.h"
-#include "BlinkM_funcs.h"
-#include <avr/pgmspace.h>  // for progmem stuff
+#include <Wire.h>
 #include <SPI.h>
+#include "BlinkM_funcs.h"
+
+#include <avr/pgmspace.h>  // for progmem stuff
 #include <RTClib.h>
 #include <RTC_DS3231.h>
-#include "TSL2561.h"
+#include <TSL2561.h>
+#include "LiquidCrystal.h"
 
 //**TODO: evauluate if I need these globals -bjt
 const boolean BLINKM_ARDUINO_POWERED = true;
@@ -18,7 +20,7 @@ byte blinkm_addr = 0x09; // the default address of all BlinkMs
 // the ADDR pin float (addr 0x39), or tie it to ground or vcc. In those cases
 // use TSL2561_ADDR_LOW (0x29) or TSL2561_ADDR_HIGH (0x49) respectively
 TSL2561 tsl(TSL2561_ADDR_FLOAT); 
-
+LiquidCrystal lcd(0);
 RTC_DS3231 RTC;
 //#define SQW_FREQ DS3231_SQW_FREQ_1024     //0b00001000   1024Hz
 //#define PWM_COUNT 1020   //determines how often the LED flips
@@ -69,7 +71,10 @@ void setup() {
     RTC.adjust(DateTime(__DATE__, __TIME__));
   }  
  
-
+  // set up the LCD's number of rows and columns: 
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  lcd.print("Anti Predator Device");
   
   
   lookForBlinkM();
@@ -79,28 +84,31 @@ void setup() {
 
 void loop() {
  
+   lcd.setCursor(0, 1);
   BlinkM_fadeToRandomRGB( blinkm_addr, '100','100','100');
   DateTime now = RTC.now();
+  
    uint16_t x = tsl.getLuminosity(TSL2561_VISIBLE);     
+   int luminosity = x;
     //uint16_t x = tsl.getLuminosity(TSL2561_FULLSPECTRUM);
     //uint16_t x = tsl.getLuminosity(TSL2561_INFRARED);
    
-    Serial.print(now.year(), DEC);
-    Serial.print('/');
-    Serial.print(now.month(), DEC);
-    Serial.print('/');
-    Serial.print(now.day(), DEC);
-    Serial.print(' ');
-    Serial.print(now.hour(), DEC);
-    Serial.print(':');
-    Serial.print(now.minute(), DEC);
-    Serial.print(':');
-    Serial.print(now.second(), DEC);
-    Serial.println();
+    lcd.print(now.year(), DEC);
+    lcd.print('/');
+    lcd.print(now.month(), DEC);
+    lcd.print('/');
+    lcd.print(now.day(), DEC);
+    lcd.print(' ');
+    lcd.print(now.hour(), DEC);
+    lcd.print(':');
+    lcd.print(now.minute(), DEC);
+    lcd.print(':');
+    lcd.print(now.second(), DEC);
+    lcd.setCursor(21,3);
    
-  
-    Serial.println(x, DEC);
-    delay(1000);
+    //lcd.print('Luminosity: ');
+    lcd.print(luminosity);
+    delay(100);
   
 }
 

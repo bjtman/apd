1fecbd5dc2ee9acfc61744c70566e32db5c49a77
bjtice <bjtice@gmail.com>
2014-07-30 21:41:34 +0100

    Added support for dual SD, VS1053

--- apd.ino
+++ apd.ino
@@ -11,6 +11,7 @@
 #include <RTClib.h>
 #include <RTC_DS3231.h>
 #include <TSL2561.h>
+#include <Adafruit_VS1053.h>
 #include "LiquidCrystal.h"
 
 //**TODO: evauluate if I need these globals -bjt
@@ -35,6 +36,26 @@ RTC_DS3231 RTC;
 
 //volatile long TOGGLE_COUNT = 0;
 
+// These are the pins used for the breakout example
+#define BREAKOUT_RESET  9      // VS1053 reset pin (output)
+#define BREAKOUT_CS     10     // VS1053 chip select pin (output)
+#define BREAKOUT_DCS    8      // VS1053 Data/command select pin (output)
+// These are the pins used for the music maker shield
+#define SHIELD_CS     7      // VS1053 chip select pin (output)
+#define SHIELD_DCS    6      // VS1053 Data/command select pin (output)
+
+// These are common pins between breakout and shield
+#define CARDCS 4     // Card chip select pin
+// DREQ should be an Int pin, see http://arduino.cc/en/Reference/attachInterrupt
+#define DREQ 3       // VS1053 Data request, ideally an Interrupt pin
+
+Adafruit_VS1053_FilePlayer musicPlayer = 
+  // create breakout-example object!
+  Adafruit_VS1053_FilePlayer(BREAKOUT_RESET, BREAKOUT_CS, BREAKOUT_DCS, DREQ, CARDCS);
+  // create shield-example object!
+  //Adafruit_VS1053_FilePlayer(SHIELD_CS, SHIELD_DCS, DREQ, CARDCS);
+
+//DATALOGGER STUFF:
 const int chipSelect = 53;
 
 File dataFile;
@@ -111,12 +132,69 @@ void setup() {
     // Wait forever since we cant write data
     while (1) ;
   }
+  
+  if (! musicPlayer.begin()) { // initialise the music player
+        Serial.println(F("Couldn't find VS1053, do you have the right pins defined?"));
+        while (1);
+  }
+  Serial.println(F("VS1053 found"));
    
+  // Set volume for left, right channels. lower numbers == louder volume!
+  musicPlayer.setVolume(20,20);
+
+  // Timer interrupts are not suggested, better to use DREQ interrupt!
+  //musicPlayer.useInterrupt(VS1053_FILEPLAYER_TIMER0_INT); // timer int
+
+  // If DREQ is on an interrupt pin (on uno, #2 or #3) we can do background
+  // audio playing
+  musicPlayer.useInterrupt(VS1053_FILEPLAYER_PIN_INT);  // DREQ int
+   musicPlayer.startPlayingFile("track002.mp3");
 }
 
 void loop() {
   
- 
+  
+   if (Serial.available()) {
+    char c = Serial.read();
+    
+    // if we get an 's' on the serial console, stop!
+    if (c == 's') {
+      musicPlayer.stopPlaying();
+      Serial.println("Done playing music");
+      SD.end();
+      delay(1000);
+      SD.begin(chipSelect);
+      dataFile = SD.open("datalog.txt", FILE_WRITE);
+    }
+    
+    if (c == 'g') {
+      
+      // need to switch SD card's here. stop logging. starting playing mp3
+      SD.end();
+      delay(100);
+     
+      SD.begin(CARDCS);    // initialise the SD card
+  
+      Serial.println(F("Playing track 002"));
+      musicPlayer.startPlayingFile("track002.mp3");
+    }
+      
+    // if we get an 'p' on the serial console, pause/unpause!
+    if (c == 'p') {
+      if (! musicPlayer.paused()) {
+        Serial.println("Paused");
+        musicPlayer.pausePlaying(true);
+      } else { 
+        Serial.println("Resumed");
+        musicPlayer.pausePlaying(false);
+      }
+    }
+  }
+  
+  
+  
+  
+ if (musicPlayer.stopped()) {
    lcd.setCursor(0, 1);
   BlinkM_fadeToRandomRGB( blinkm_addr, '100','100','100');
   DateTime now = RTC.now();
@@ -167,7 +245,7 @@ void loop() {
     // SD card is filled with data.
     dataFile.flush();
     delay(500);
-  
+ }
 }
 
 void lookForBlinkM() {

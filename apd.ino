8950c9ad199ec876ec2d3275e6001b3a17901ff1
bjtice <bjtice@gmail.com>
2014-08-01 00:04:44 +0100

    Added PIR sensor support

--- apd.ino
+++ apd.ino
@@ -1,4 +1,4 @@
-// APD version 1.1.04
+// APD version 1.1.05
 // 7/31/14
 // Brian Tice
 
@@ -70,6 +70,26 @@ const int chipSelect = 53;
 
 File dataFile;
 
+//PIR sensor's setup variables
+
+/////////////////////////////
+//VARS
+//the time we give the sensor to calibrate (10-60 secs according to the datasheet)
+int calibrationTime = 30;
+
+//the time when the sensor outputs a low impulse
+long unsigned int lowIn;         
+
+//the amount of milliseconds the sensor has to be low 
+//before we assume all motion has stopped
+long unsigned int pause = 5000;  
+
+boolean lockLow = true;
+boolean takeLowTime;  
+
+const int pirPin = 2;    //the digital pin connected to the PIR sensor's output
+const int ledPin = 40;   //Motion indicated on PIR 1
+
 
 void setup() {
   
@@ -90,6 +110,23 @@ void setup() {
   pinMode(button3, INPUT);
   pinMode(button4, INPUT);
   pinMode(button5, INPUT);
+  
+  // Setup PIR sensor 
+  pinMode(pirPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+
+  digitalWrite(pirPin, LOW);
+
+  //give the sensor some time to calibrate
+  Serial.print("calibrating sensor ");
+    for(int i = 0; i < calibrationTime; i++){
+      Serial.print(".");
+      delay(1000);
+      }
+    Serial.println(" done");
+    Serial.println("SENSOR ACTIVE");
+    delay(50);
+  
   
   if( BLINKM_ARDUINO_POWERED )
     BlinkM_beginWithPower();
@@ -205,9 +242,53 @@ void loop() {
       if (! musicPlayer.paused()) {
         Serial.println("Paused");
         musicPlayer.pausePlaying(true);
-        digitalWrite(PIEZOSOUNDERPIN,HIGH);
-        delay(500);
-        digitalWrite(PIEZOSOUNDERPIN,LOW);
+        
+        // *** PIEZO READ ROUTINE
+        // 
+        if(digitalRead(pirPin) == HIGH){
+          digitalWrite(ledPin, HIGH);   //the led visualizes the sensors output pin state
+      
+          if(lockLow){  
+            //makes sure we wait for a transition to LOW before any further output is made:
+            lockLow = false;            
+            Serial.println("---");
+            Serial.print("motion detected at ");
+            Serial.print(millis()/1000);
+            Serial.println(" sec"); 
+            delay(50);
+            // PIEZO SENSOR ACTUATION ROUTINE
+            //
+            digitalWrite(PIEZOSOUNDERPIN,HIGH);
+            delay(500);
+            digitalWrite(PIEZOSOUNDERPIN,LOW);
+            //
+            // DONE PIEZO
+          }         
+          takeLowTime = true;
+        }
+
+        if(digitalRead(pirPin) == LOW){       
+          digitalWrite(ledPin, LOW);  //the led visualizes the sensors output pin state
+      
+          if(takeLowTime){
+            lowIn = millis();          //save the time of the transition from high to LOW
+            takeLowTime = false;       //make sure this is only done at the start of a LOW phase
+          }
+          //if the sensor is low for more than the given pause, 
+          //we assume that no more motion is going to happen
+          if(!lockLow && millis() - lowIn > pause){  
+           //makes sure this block of code is only executed again after 
+           //a new motion sequence has been detected
+           lockLow = true;                        
+           Serial.print("motion ended at ");      //output
+           Serial.print((millis() - pause)/1000);
+           Serial.println(" sec");
+           delay(50);
+          }
+       }
+        
+        
+        
         
       } else { 
         Serial.println("Resumed");
